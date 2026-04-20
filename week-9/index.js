eeaed67912dc868637cb65b3c7060cebed463d7e
Ayush gopal <ayushgopal17@gmail.com>
2026-04-20 20:09:32 +0530

    adding jwt

--- week-9/index.js
+++ week-9/index.js
@@ -1,10 +1,57 @@
 
 
-const express=require("express")
+const express=require("express");
+const jwt=require("jsowebtoken");
+
+
 const app=express()
+
 app.use(express.json());
 
 const notes=[];
+const users=[];
+
+app.post("/signup",(req,res)=>{
+    const username=req.body.username;
+    const password=req.body.password;
+    const userExists= user.find(user=> user.username === username);
+    if(userExists) {
+        return res.status(403).json({
+            message: "User with username alreaduy exist"
+        })
+    }
+
+    users.push({
+        username: username ,
+        password: password
+    })
+    res.json({
+        message: "you have signup"
+    })
+})
+
+app.post("/signin",(req,res)=>{
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const userExists=user.find(user => user.username === username && user.password === password);
+
+    if(!username){
+        res.status(403).json({
+            message: "Incorrect Creditential"
+        })
+            return;
+    }
+const token= jwt.sign({
+
+username: username
+}, "Ayush123")
+res.json({
+    token:token;
+})
+})
+
+
 
 app.post("/notes",(req,res)=>{
     
