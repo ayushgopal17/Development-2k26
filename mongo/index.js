6866a4b982a99a8ed4466d300ece808f5fca8eb0
Ayush gopal <ayushgopal17@gmail.com>
2026-04-30 19:33:15 +0530

    todo with mongo

--- mongo/index.js
+++ mongo/index.js
@@ -2,20 +2,105 @@ const express= require ('express')
 const { authMiddleware } = require('./middleware')
 const app=express()
 const jwt= require('jsonwebtoken')
-app.use(express.json())
+app.use(express.json());
 
-app.post('/signup',(req,res)=>{
+let USERS=[];
+let TODOS=[];
+let CURRENT_TODO_ID=1;
 
+app.post("/signup",(req,res)=>{
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const existingUser= USERS.find(u=> u.username=== username);
+        if(existingUser){
+            res.status(403).send({
+                message: "user already exist please signin"
+            })
+            return
+        }
+        USERS.push({
+            id: CURRENT_TODO_ID++,
+            username: username,
+            password: password
+        })
+
+        res.json({
+            id: CURRENT_TODO_ID-1
+        })
+
+    
 })
+
 app.post('/signin',(req,res)=>{
+
+      const username=req.body.username;
+    const password=req.body.password;
+
+    const existingUser= USERS.find(u=> u.username=== username && u.password ===password);
+        if(!existingUser){
+            res.status(403).send({
+                message: "INcorrect credientials"
+            })
+            return
+        }
+       const token =jwt.sign({
+        userId: existingUser.id
+       },"Ayush123")
+
+       res.json({
+        token
+       })
 
 })
 app.post('/todo',authMiddleware,(req,res)=>{
 
     const userId= req.userId;
+   const title=req.body.title;
+   const description=req.body.description;
+   TODOS.push({
+    id: CURRENT_TODO_ID++,
+    title,
+    description,
+    userId
+   })
+   res.json({
+    message: "Tood created successfully"
+   })
+
 })
+
+app.delete('/todo/:todoId',authMiddleware,(req,res)=>{
+
+     const userId=req.userId;
+     const todoId=parseInt(req.params.todoId);
+
+    const doesUserOwnsTodo =TODOS.find(t=> t.id=== todoId && t.userId === userId );
+  
+    if(doesUserOwnsTodo){
+        TODOS= TODOS.filter(t => t.id= todoId);
+        res.json({
+            message: "Todo Deleted"
+        })
+    }
+    else{
+        res.json({
+            message: "Either Todo don't exist or it is not your todo"
+        })
+    }
+
+
+})
+
+
 app.get('/todos',authMiddleware,(req,res)=>{
  const userId=req.userId;
+
+ const userTodos=TODOS.filter(t=> t.userId=== userId);
+
+ res.json({
+    todos: userTodos
+ })
 })
 
 
--- mongo/middleware.js
+++ mongo/middleware.js
@@ -1,30 +1,52 @@
-const jwt= require("jsonwebtoken")
+// const jwt= require("jsonwebtoken")
+
+// function authMiddleware(req,res,next){
+//     const token= req.headers.token;
+
+
+//     if(!token) {
+//         req.status(403).json({
+//             message: "you are not logged in"
+//         });
+//         return;
+//     }
+
+//     if(decode.userId){
+//     const decode= jwt.verify(token,"Ayush123");
+//     next();
+//     }
+//     else{
+//         req.status(403).json({
+//             message: "token invalid or not found"
+//         })
+//     }
+
+
+
+// }
+
+// module.exports = { authMiddleware: authMiddleware}
+
+
+const jwt=require('jsonwebtoken')
 
 function authMiddleware(req,res,next){
-    const token= req.headers.token;
+   const token=req.headers.token;
+   
+   const decode=jwt.verify(token,"Ayush123");
+   const userId=parseInt(decode.userId);
 
-
-    if(!token) {
-        req.status(403).json({
-            message: "you are not logged in"
-        });
-        return;
-    }
-
-    if(decode.userId){
-    const decode= jwt.verify(token,"Ayush123");
+   if(userId){
+    
+    req.userId=userId;
     next();
-    }
-    else{
-        req.status(403).json({
-            message: "token invalid or not found"
-        })
-    }
-
-
-
+   }
+   else{
+    res.status(403).json({
+        message: "Token was incorrect"
+    })
+   }
 }
 
-module.exports = { authMiddleware: authMiddleware}
-
+module.exports={authMiddleware}
 
